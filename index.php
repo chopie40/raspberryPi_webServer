87ae05560e69cf0a78a4c408d708a30247d3d57c
DentedDiamond <71598060+chopie40@users.noreply.github.com>
2025-08-28 23:40:37 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="fr-ca">
+<head>
+    <title>GatineauÉlectro | Réparation d'ordinateurs et d'équipements électroniques à Gatineau et Ottawa</title>
+	<meta charset="UTF-8">	
+	<link rel="icon" type="image/x-icon" href="images/tape_32x32.ico">
+	<meta name="viewport" content="initial-scale=1, width=device-width">
+	<meta name="keywords" content="gatineauelectro, réparation, dépannage, remise à neuf, diagnostic, électronique, ordinateur, portable, condensateur, radio, am, fm, téléphone, cellulaire, cassette, lecteur, cd, cd-rom, dvd, blue-ray, magnétoscope, vhs, télévision, écran, cathodique, téléviseur, smart tv, lcd, robotique, programmation">
+	<meta name="description" content="Réparation d'ordinateurs et d'équipements électroniques de confiance à Gatineau et Ottawa.">
+	<link rel="stylesheet" href="styles.css">
+	<link rel="stylesheet" href="w3.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+    .w3-bar .w3-button {
+        padding: 16px;
+    }
+    </style>
+</head>
+<body>
+    <div class="w3-top">
+        <div class="w3-bar w3-black">
+            <a href="index.php" class="w3-bar-item">
+                <img src="images/LogoTape_32x32.png" alt="Logo du site web représentant une cassette audio.">
+            </a>
+            <div class="w3-right w3-hide-small">
+                <a href="about.html" class="w3-bar-item">À propos</a>
+                <a href="work.html" class="w3-bar-item">Les projets</a>
+                <a href="video.html" class="w3-bar-item">Vidéothèque</a>
+                <a href="reservation.html" class="w3-bar-item">Réservation</a>
+            </div>
+            <a href="javascript:void(0)" class="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium" onclick="w3_open()">
+                <i class="fa fa-bars"></i>
+            </a>
+        </div>
+    </div>
+
+    <nav class="w3-sidebar w3-bar-block w3-black w3-hide-medium w3-hide-large" style="display:none" id="mySidebar">
+        <a href="javascript:void(0)" onclick="w3_close()" class="w3-bar-item w3-button w3-padding-16">X</a>
+        <a href="about.html" onclick="w3_close()" class="w3-bar-item w3-button">À propos</a>
+        <a href="work.html" onclick="w3_close()" class="w3-bar-item w3-button">Les projets</a>
+        <a href="video.html" onclick="w3_close()" class="w3-bar-item w3-button">Vidéothèque</a>
+        <a href="reservation.html" onclick="w3_close()" class="w3-bar-item w3-button">Prise de rendez-vous</a>
+    </nav>
+
+    <!-- Header with full-height image -->
+    <header class="bgimg-1 w3-display-container w3-grayscale-min" id="home">
+        <div class="w3-display-left w3-text-sand" style="padding:32px">
+            <span class="w3-xxxlarge">Réparation d'ordinateurs et d'équipements électroniques à Gatineau et Ottawa</span><br>
+            <span class="w3-xlarge">Vous souhaitez en savoir plus</span>
+            <p>
+                <a href="about.html" class="w3-button w3-gray w3-padding-large w3-large w3-margin-top">
+                    Renseignements pertinents
+                </a>
+            </p>
+        </div>
+    </header>
+
+    <!-- Footer -->
+    <footer class="w3-container w3-bottom w3-center w3-black w3-padding-top-24">
+        <div>
+            <a class="fa fa-facebook" href="https://www.facebook.com/profile.php?id=723906274141004" target="_blank"></a>
+            <a class="fa fa-whatsapp" href="https://wa.me/message/SKTYI5LDWAADL1" target="_blank"></a>
+        </div>
+        <p>2025<br>Développé par DentedDiamond et Nympha</p>
+    </footer>
+	<script src="sideBar.js"></script>
+</body>
+</html>
